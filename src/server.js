ce0947f262a9118d7f00eb786b5308f6cc5f60f8
Lucian Bellini <lucianbellini1@gmail.com>
2023-07-29 18:52:27 -0300

    Setting database connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+dotenv.config();
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
@@ -10,3 +12,18 @@ app.listen(process.env.PORT, (err) => {
     console.log(`Server listening on http://localhost:3000`);
   }
 });
+
+const connectToDb = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      //para evitar erros
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    //Depois disso, já que o mongoose.conect é uma função assíncrona, vai dizer que tá conectado ou...
+    .then(() => console.log("MongoDB Atlas connected"))
+    //se tiver um erro, vai mostrar qual o erro
+    .catch((err) => console.log(err));
+};
+
+connectToDb();
